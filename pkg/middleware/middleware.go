6467019f0e032afd78f269fad5e22f7b27178884
Archer <archer@mayflower.work>
2025-07-04 15:00:14 +0300

    feat: FIFO middlewares helper function

--- pkg/middleware/middleware.go
+++ pkg/middleware/middleware.go
@@ -4,11 +4,20 @@ import (
 	"github.com/valyala/fasthttp"
 )
 
-type Funcs []Func
+type Middlewares []Middleware
 
-type Func func(next fasthttp.RequestHandler) fasthttp.RequestHandler
+type Middleware func(next fasthttp.RequestHandler) fasthttp.RequestHandler
 
-func (f Funcs) Apply(h fasthttp.RequestHandler) fasthttp.RequestHandler {
+// Chain applies middlewares in "adding" order (FIFO)
+func (m Middlewares) Chain(h fasthttp.RequestHandler) fasthttp.RequestHandler {
+	for i := len(m) - 1; i >= 0; i-- {
+		h = m[i](h)
+	}
+	return h
+}
+
+// Apply applies middlewares in LIFO order
+func (f Middlewares) Apply(h fasthttp.RequestHandler) fasthttp.RequestHandler {
 	for _, m := range f {
 		h = m(h)
 	}
